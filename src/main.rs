18fa32f214d546b0626a64c7736ce39985896882
dewfhbsdgbfhsjbsjkdnckds <firstnamelastname596678@gmail.com>
2026-01-11 22:09:07 +0000

    breaking everything. Beginning of refactor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![allow(unused_variables, non_snake_case, non_upper_case_globals, unused_imports, unused_parens)]
+#![allow(unused_variables, non_snake_case, non_upper_case_globals, unused_imports, unused_parens, non_camel_case_types)]
 //extern crate sdl3;
 
 use sdl3::pixels::Color;
@@ -8,24 +8,45 @@ use sdl3::Sdl;
 use sdl3::VideoSubsystem;
 use sdl3::rect::Point;
 use sdl3::rect::Rect;
+use sdl3::render::Canvas;
 use sdl3::render::FPoint;
 use sdl3::render::FRect;
 use sdl3::sys::keycode::SDLK_SPACE;
 use sdl3::sys::video::SDL_WINDOW_RESIZABLE;
+use sdl3::video::Window;
+use std::ops::Deref;
+use std::ops::Index;
 use std::thread::sleep;
 use std::time::Duration;
+//use sdl3::sys::vulkan::*;
+//use sdl3::sys::vulkan::VkSurfaceKHR
 
 #[derive(Debug)]
 struct Pixels {
     color: Color,
-    points: Vec<FPoint>,
+    points: Vec<Point>,
 }
 
+// maybe just rename this to square or something idk
 struct CanvasSize {
     x1: i32,
     y1: i32,
     x2: i32,
     y2: i32,
+}
+
+struct Whiteboard {
+    canvasBounds: CanvasSize,
+    canvas: Canvas<Window>,
+    pixels: Vec<Pixels>,
+    bgcolor: Color
+}
+
+enum ToolMode {
+    none,
+    pencil,
+    eraser,
+    movingCanvas,
 }
 
 impl CanvasSize {
@@ -95,6 +116,67 @@ fn makeLine(list: &mut Vec<Point>, point1: Point, point2: Point){
     }
 }
 
+fn mouseMovement(whiteboard: &mut Whiteboard, point1: Point, point2: Point, toolMode: &ToolMode, color: Color){
+    match toolMode {
+        ToolMode::none => {
+            return;
+        },
+        ToolMode::movingCanvas => {
+            let dx = point1.x - point2.x;
+            let dy = point1.y - point2.y;
+            whiteboard.canvasBounds.shift(dx, dy);
+        },
+        ToolMode::pencil => {
+            let mut pointList: Vec<Point> = Vec::new();
+            makeLine(&mut pointList, point1, point2);
+            for point in pointList {
+                if (!whiteboard.canvasBounds.isInside(point)) {continue;}
+                let mut pointDrawn = false;
+                // i should check for duplicates here 
+                for pixels in &mut whiteboard.pixels {
+                    if (pixels.color == color) {
+                        pixels.points.push(Point::new(point.x - whiteboard.canvasBounds.x1, point.y - whiteboard.canvasBounds.y1));
+                        pointDrawn = true;
+                        break;
+                    }
+                }
+                if (!pointDrawn) {
+                    let pixels = Pixels {points: vec![Point::new(point.x - whiteboard.canvasBounds.x1, point.y - whiteboard.canvasBounds.y1)], color};
+                    whiteboard.pixels.push(pixels);
+                }
+                whiteboard.canvas.set_draw_color(color);
+                let result = whiteboard.canvas.draw_point(point);
+            }
+        },
+        ToolMode::eraser => {
+            // i really hope theres a more pretty way to do this :sob:
+            let mut pointList: Vec<Point> = Vec::new();
+            makeLine(&mut pointList, point1, point2);
+            for eraserPoint in pointList {
+                if (!whiteboard.canvasBounds.isInside(eraserPoint)) {continue;}
+                //let mut pixelErased = false;
+                'inner: for pixels in &mut whiteboard.pixels {
+                    for i in 0..pixels.points.len() {
+                        if (pixels.points.get(i).is_some()){
+                            let point = pixels.points.get(i).unwrap();
+                            if (eraserPoint == *point) {
+                                // swap remove is faster and i dont need order
+                                pixels.points.swap_remove(i);
+                                whiteboard.canvas.set_draw_color(color);
+                                let result = whiteboard.canvas.draw_point(eraserPoint);
+                                break 'inner;
+                            } 
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
+
+
+
 
 // todo
 // undo feature
@@ -105,8 +187,8 @@ fn main() {
     let sdl_context: Sdl = sdl3::init().unwrap();
     let video_subsystem:VideoSubsystem = sdl_context.video().unwrap();
 
-    const width: u32 = 1800;
-    const height: u32 = 1000;
+    const width: u32 = 900;
+    const height: u32 = 500;
     const bgcolor: Color = Color::RGB(40, 40, 40);
     let window = video_subsystem.window("whiteboard", width, height)
         .set_window_flags(SDL_WINDOW_RESIZABLE as u32) // casting? still works?
@@ -114,18 +196,18 @@ fn main() {
         .build()
         .unwrap();
 
-    let mut canvas = window.into_canvas();
-    let mut canvasBounds: CanvasSize = CanvasSize { x1: (0), y1: (0), x2: (width as i32), y2: (height as i32) };
+    let canvas = window.into_canvas();
+    let canvasBounds: CanvasSize = CanvasSize {x1: 0, y1: 0, x2: width as i32, y2: height as i32};
+    let mut whiteboard = Whiteboard {canvasBounds, canvas, pixels: Vec::new(), bgcolor};
+    let mut toolMode = ToolMode::pencil;
 
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
-    canvas.clear();
-    canvas.present();
+    whiteboard.canvas.set_draw_color(Color::RGB(0, 0, 0));
+    whiteboard.canvas.clear();
+    whiteboard.canvas.present();
 
     let mut mouseHeldDown = false;
-    let mut isMovingCanvas = false;
     let mut event_pump = sdl_context.event_pump().unwrap();
     let /*mut*/ currentColor = Color::RGB(255, 255, 255);
-    let mut PixelsVec: Vec<Pixels> = Vec::new();
     let mut point1: Point;
     let mut point2: Option<Point> = None;
     'running: loop {
@@ -133,7 +215,7 @@ fn main() {
         let mut needsClear = false;
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), ..} => {
+                Event::Quit {..} | Event::KeyDown {keycode: Some(Keycode::Escape), ..} => {
                     break 'running;
                 },
                 Event::MouseButtonDown {..} => {
@@ -144,55 +226,17 @@ fn main() {
                     point2 = None;
                 },
                 Event::KeyDown {keycode: Some(Keycode::Space), ..} => {
-                    isMovingCanvas = true;
+                    toolMode = ToolMode::movingCanvas;
                 },
                 Event::KeyUp { keycode: Some(Keycode::Space), ..} => {
-                    isMovingCanvas = false;
+                    toolMode = ToolMode::none;
                 },
-                Event::KeyDown {keycode: Some(Keycode::P), ..} => {
-                    println!("length of pixels is {}", PixelsVec.len());
-                    println!("contents of pixels is {:?}", PixelsVec);
-                }
-                Event::MouseMotion {x, y, ..} => 'mouse: {
-                    // this might be a little too much indentation lol
-                    if (!mouseHeldDown){break 'mouse;}
+                Event::MouseMotion {x, y, ..} => {
                     point1 = Point::new(x as i32, y as i32);
-                    if (isMovingCanvas) {
-                        needsDraw = true;
-                        needsClear = true;
-                        if (point2.is_some()){
-                            let dx = point1.x - point2.unwrap().x;
-                            let dy = point1.y - point2.unwrap().y;
-                            canvasBounds.shift(dx, dy);
-                        }
-                        point2 = Some(point1);
-                        break 'mouse;
-                    }
-                    point1 = Point::new(x as i32, y as i32);
-                    if (point2.is_some()) {
-                        let mut pointList: Vec<Point> = Vec::new();
-                        makeLine(&mut pointList, point1, point2.unwrap());
-                        for point in pointList {
-                            if (canvasBounds.isInside(point)) {
-                                let mut pushedPoint = point.clone();
-                                pushedPoint.x -= canvasBounds.x1;
-                                pushedPoint.y -= canvasBounds.y1;
-                                let mut pointDrawn = false;
-                                for pixels in &mut PixelsVec {
-                                    if (pixels.color == currentColor) {
-                                        pixels.points.push(pushedPoint.into());
-                                        pointDrawn = true;
-                                        break;
-                                    }
-                                }
-                                if (!pointDrawn) {
-                                    let pixels = Pixels {points: vec![pushedPoint.into()], color: currentColor};
-                                    PixelsVec.push(pixels);
-                                }
-                                canvas.set_draw_color(currentColor);
-                                let result = canvas.draw_point(point);
-                            }
-                        }
+                    needsDraw = true;
+                    needsClear = true;
+                    if (mouseHeldDown && point2.is_some()) {
+                        mouseMovement(&mut whiteboard, point1, point2.unwrap(), &toolMode, currentColor);
                     }
                     point2 = Some(point1);
                 },
@@ -200,22 +244,22 @@ fn main() {
             }
         }
         if (needsClear){
-            canvas.set_draw_color(bgcolor);
-            canvas.clear();
-            canvas.set_draw_color(Color::RGB(0, 0, 0));
-            let result = canvas.fill_rect(FRect::new(canvasBounds.x1 as f32, canvasBounds.y1 as f32, canvasBounds.width() as f32, canvasBounds.height() as f32));
+            whiteboard.canvas.set_draw_color(bgcolor);
+            whiteboard.canvas.clear();
+            whiteboard.canvas.set_draw_color(Color::RGB(0, 0, 0));
+            let result = whiteboard.canvas.fill_rect(FRect::new(whiteboard.canvasBounds.x1 as f32, whiteboard.canvasBounds.y1 as f32, whiteboard.canvasBounds.width() as f32, whiteboard.canvasBounds.height() as f32));
         }
         if (needsDraw){
-            let oldViewport = canvas.viewport();
-            canvas.set_viewport(Rect::new(canvasBounds.x1, canvasBounds.y1, canvasBounds.width() as u32, canvasBounds.height() as u32));
-            for pixels in &PixelsVec {
-                canvas.set_draw_color(pixels.color);
-                let result2 = canvas.draw_points(pixels.points.as_slice());
-            }
-            canvas.set_viewport(oldViewport);
+            let oldViewport = whiteboard.canvas.viewport();
+            whiteboard.canvas.set_viewport(Rect::new(whiteboard.canvasBounds.x1, whiteboard.canvasBounds.y1, whiteboard.canvasBounds.width() as u32, whiteboard.canvasBounds.height() as u32));
+            //for pixels in &PixelsVec {
+            //    canvas.set_draw_color(pixels.color);
+            //    //let result2 = canvas.draw_points(pixels.points.as_slice());
+            //}
+            whiteboard.canvas.set_viewport(oldViewport);
         }
         // call at the end of every loop
-        canvas.present();
+        whiteboard.canvas.present();
         sleep(Duration::new(0, 500_000_000u32 / 60));
     }
 }
